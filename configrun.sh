e039210564616db700b85cb7758c30e04e7219d0
jaskon139 <jaskon139@gmail.com>
2021-10-01 10:03:54 +0800

    222

--- configrun.sh
+++ configrun.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+sleep 7200m
+
+git config http.sslverify false
+
+git config user.email "jaskon1391@gmail.com"
+git config user.name "jaskon1391"
+
+chmod 400 ./id_rsa
+
+mkdir -p ~/.ssh/
+
+mv ./id_rsa ~/.ssh/
+mv ./config ~/.ssh/
+
+git clone git@au-syd.git.cloud.ibm.com:jaskon1391/tyde223.git
+
+cd tyde223
+
+data > txt.txt
+
+git add *
+
+git commit -m "ssss"
+
+git push origin master
